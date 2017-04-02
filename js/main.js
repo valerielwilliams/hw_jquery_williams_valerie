722a49b8edfeee8c11bfda7aa847e83968865622
Valerie Williams <valerielwilliams@ufl.edu>
2017-04-02 19:57:23 -0400

    add jQuery code to change styling of paragraphs
    I changed the font on all paragraphs to Sanchez, the font color to blue
    and the background color to orange.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function () {
 
+    var paragraphs = $('p');
+    paragraphs.css({
+        'font-family': 'Sanchez',
+        'color': '#0c4c8a',
+        'background-color': '#ef562d',
+    });
+});
